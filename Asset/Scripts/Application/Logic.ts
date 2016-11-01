935af63d3b11f926d25a95eff9a7a3d0430a3166
ari.heinonen@stud.pointcollege.fi <ari.heinonen@stud.pointcollege.fi>
2016-11-01 07:04:17 +0200

    Assetin assignlocation toimii

--- Asset/Scripts/Application/Logic.ts
+++ Asset/Scripts/Application/Logic.ts
@@ -1,7 +1,36 @@
 ﻿/// <reference path="../typings/jquery/jquery.d.ts" />
+class AssignLocationModel {
+    public AssetCode: string;
+    public LocationCode: string;
+}
+
 
 function initAssetAssignment() {
     $("#AssignAssetButton").click(function () {
-        alert("Toimii!");
+        var locationCode: string = $("#LocationCode").val();
+        var assetCode: string = $("#AssetCode").val();
+
+        alert("L: " + locationCode + ", A: " + assetCode);
+        var data: AssignLocationModel = new AssignLocationModel();
+        data.LocationCode = locationCode;
+        data.AssetCode = assetCode;
+
+        $.ajax({
+            type: "POST",
+            url: "/Asset/AssignLocation",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            success: function (data) {
+                if (data.success == true) {
+                    alert("Asset successfully assigned.");
+                }
+                else {
+                    alert("There was an error: " + data.error);
+                }
+            },
+            dataType: "json"
+        });
     });
 }
+
+ 
